0cd75bab8e98e0cb939259bc65217be982b28a60
isseikz <kuzumaki-issei-sk@ynu.jp>
2019-05-23 17:26:36 +0900

    rename

--- test_est_system_with_PID.py
+++ test_est_system_with_PID.py
@@ -9,7 +9,7 @@ from sub_system import equation_of_motion as em
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.models import model_from_json
+from keras.models import load_model
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers.recurrent import LSTM
@@ -22,26 +22,28 @@ import pickle
 import datetime
 
 
+def input_range(x):
+    if x > 1.0:
+        return 1.0
+    elif x < 0.0:
+        return 0.0
+    else:
+        return x
+
+
 def main():
-    tz_jst = datetime.timezone(datetime.timedelta(hours=9))
-    dt_now = datetime.datetime.now(tz=tz_jst)
-    file_date = dt_now.strftime('%Y%m%d%H%M%S')
+    file_date = '20190520164121'
 
     # --- load model ---
         #  If you want to load a pre-designed model, then uncomment.
-    with open('./model/Seminar 190516/20190516000814_target_model.bin', mode='rb', ) as f:
-        model = pickle.load(f)
-        # If you want to load a pre-designed model.
+    # with open('./model/'+file_date+'_target_model.bin', mode='rb') as f:
+    #     model = pickle.load(f)
+    model = frame.Multicopter()
+    model.read_regular_settings_zhang()
 
-        # If you want to create a new model, then uncomment.
-    # model = frame.Multicopter()
-    # model.read_regular_settings()
-        # If you want to create a new model.
     # --- load model ---
 
     model.show_settings()
-    # with open('./model/'+file_date+'_target_model.bin', 'wb') as f: # save model
-    #     pickle.dump(model, f, protocol=4)
 
     print(f'Initial position: {model.get_position()}')
     print(f'Initial velocity: {model.get_velocity()}')
@@ -65,10 +67,9 @@ def main():
     n_sequences = 10
     input_dim = (n_input + n_states) * n_sequences + n_input
 
-    with open('./model/Seminar 190516/20190516000814_est_model.json', 'r') as f:
-        # model_json = json.loads(f.read())
-        estimator = model_from_json(f.read())
-    estimator.load_weights('./model/Seminar 190516/20190516000814_est_model_weights.hdf5')
+    # Load pre-designed estimator
+    estimator = load_model('./model/'+file_date+'_estimator_model.h5')
+    # load pre-designed estimator
 
     batch_x = np.zeros((n_sequences, n_input+n_states))
     batch_y = np.zeros((n_sequences, n_states))
@@ -81,14 +82,6 @@ def main():
     pred_state = em.SixDOF(0.0, 0.01)
     # --- estimated simulator ---
 
-    # PD Control
-    time = 0
-    integral = 0.0
-    ref_pos = [0.5, -1.0, -1.0]
-    ref_yaw = 0.0
-    ref_final_yaw = 0.0 # math.atan2(ref_pos[1], ref_pos[0]) * 180 / (2 * math.pi)
-    # PD control
-
     history = []
     log_est = []
     log_ans = []
@@ -99,7 +92,7 @@ def main():
     log_ans_s = []
     log_ans_q = []
 
-    while time <1000: # 1 sec/ 100steps
+    while time <1001: # 1 sec/ 100steps
         if time % 200 == 0:
             print(model.dynamics.get_time())
             # model.show_status()
@@ -113,7 +106,7 @@ def main():
 
         # --- when certain time has passed or the vihicle is in dangerous state,
         #         the vehicle will be stopped and the state will be reset ---
-        if any( abs(angle) > np.pi/3 for angle in model.get_euler_angle()):
+        if (time % 100 == 0) | any( abs(angle) > np.pi/3 for angle in model.get_euler_angle()):
             print("--- Reset Vehicle Motion ---")
             model.reset_all()
             pred_state.reset_state()
@@ -122,31 +115,6 @@ def main():
             batch_y = np.zeros((n_sequences, n_states))
         # --- when certain time has passed or the vihicle is in dangerous state,
         #         the vehicle will be stopped and the state will be reset ---
-
-        # --- update input to the estimator: {x} ---
-        batch_x[0:-2, :] = batch_x[1:-1, :]
-        batch_x[-1, :] = np.hstack((dnn_input, dnn_output))
-        dnn_input = arr_inputs
-        x = np.reshape((batch_x), (n_sequences * (n_input + n_states)))
-        x = np.hstack((x, dnn_input))
-        x = np.reshape(x, (1,input_dim))
-        # --- update input to the estimator: {x} ---
-
-        pred_accang = estimator.predict(x)[0]
-        pred_acc = pred_accang[0:3]
-        pred_ang = pred_accang[3:6] / 180 * np.pi
-        pred_q = pred_state.get_quartanion()
-        pred_acc_and_g = mf.convert_vector_body_to_inertial(pred_acc, pred_q) + np.array([0.0,0.0,9.81])
-        print('est: ', pred_ang)
-        pred_state.step(np.hstack((pred_acc_and_g, pred_ang)))
-        log_est_s.append(pred_state.get_status())
-        log_est_q.append(pred_state.get_euler_angle())
-
-        # --- log current input and estimated response to update the estimator ---
-        log_est.append(np.reshape(pred_accang, 6))
-        log_est_t.append(model.dynamics.get_time())
-        log_input.append(dnn_input)
-        # --- log current input and estimated response to update the estimator ---
 
         # Calc input with PD method
         # Change nominal positoin
@@ -178,10 +146,12 @@ def main():
         attitude_deg = np.array(attitude) * 180 / math.pi
         ang_vel = model.get_angular_velocity()
 
+        k_p = 3.0
+        k_d = 0.99
         integral += (-1.0-altitude)
         inp_alt = -0.5 * (-1.0-altitude) -0.001 * integral - 0.5*(-vel_alt)
-        inp_rol = 4.0 * ( ref_roll - attitude_deg[0])/180 - 4 *0.99 * ang_vel[0]/(2*math.pi)
-        inp_pit = 4.0 * ( ref_pitch - attitude_deg[1])/180 - 4 *0.99 * ang_vel[1]/(2*math.pi)
+        inp_rol = k_p * ( ref_roll - attitude_deg[0])/180 - k_p * k_d * ang_vel[0]/(2*math.pi)
+        inp_pit = k_p * ( ref_pitch - attitude_deg[1])/180 - k_p * k_d * ang_vel[1]/(2*math.pi)
         inp_yaw = 1.0 * ( ref_yaw - attitude_deg[2])/180 - 1.0 * 0.99 * ang_vel[2]/(2*math.pi)
 
         new_inputs = np.array([
@@ -190,11 +160,35 @@ def main():
             inp_alt - inp_rol - inp_pit + inp_yaw,
             inp_alt - inp_rol + inp_pit - inp_yaw
         ]) # 左後，左前，右後，右前
+        new_inputs = np.frompyfunc(input_range, 1, 1)(new_inputs)
         arr_inputs = new_inputs
         # arr_inputs = arr_inputs * 0.5 + new_inputs * 0.5
         # print(arr_inputs, new_inputs)
         # Calc input with PD
 
+        # --- update input to the estimator: {x} ---
+        batch_x[0:-2, :] = batch_x[1:-1, :]
+        batch_x[-1, :] = np.hstack((dnn_input, dnn_output))
+        dnn_input = arr_inputs
+        x = np.reshape((batch_x), (n_sequences * (n_input + n_states)))
+        x = np.hstack((x, dnn_input))
+        x = np.reshape(x, (1,input_dim))
+        # --- update input to the estimator: {x} ---
+
+        pred_accang = estimator.predict(x)[0]
+        pred_acc = pred_accang[0:3]
+        pred_ang = pred_accang[3:6]
+        pred_q = pred_state.get_quartanion()
+        pred_acc_and_g = mf.convert_vector_body_to_inertial(pred_acc, pred_q) + np.array([0.0,0.0,9.81])
+        pred_state.step(np.hstack((pred_acc_and_g, pred_ang)))
+        log_est_s.append(pred_state.get_status())
+        log_est_q.append(pred_state.get_euler_angle())
+
+        # --- log current input and estimated response to update the estimator ---
+        log_est.append(np.reshape(pred_accang, 6))
+        log_est_t.append(model.dynamics.get_time())
+        log_input.append(dnn_input)
+        # --- log current input and estimated response to update the estimator ---
 
         # --- Plant ---
         #   You should NOT modify below in the loop
@@ -209,9 +203,9 @@ def main():
             sensor_acc
             - mf.convert_vector_inertial_to_body(
                 model.gravity,
-                model.get_quartanion())
+                model.get_quartanion()) / 9.81
             ,
-            model.get_angular_acceleration() ))
+            model.get_angular_acceleration()))
 
         batch_y[0:-2, :] = batch_y[1:-1, :]
         batch_y[-1,:] = dnn_output
@@ -223,21 +217,22 @@ def main():
 
     # simulation loop has been finished!
 
-    # --- save model and weights of the neural network
-    print('save the architecture of a model')
-    f_model = './model'
-    json_string = estimator.to_json()
-    open(os.path.join(f_model,file_date+'_est_model.json'), 'w').write(json_string)
-    print('save weights')
-    estimator.save_weights(os.path.join(f_model,file_date+'_est_model_weights.hdf5'))
-    # --- save model and weights of the neural network
-
     # Visulize machine response
-    log.visualize_data(save=True, filename=file_date)
+    log.visualize_data(save=False, show=False, filename=file_date)
     # Visulize machine response
 
     # --- Visulize the performance of the estimator ---
-    log_error = np.array(log_est) -np.array(log_ans)
+    test_object = '_test_PID'
+
+    log_est = np.array(log_est)
+    log_ans = np.array(log_ans)
+
+    log_est[0:3] *= 9.81
+    log_ans[0:3] *= 9.81
+    log_est[3:6] *= 180.0 / np.pi
+    log_ans[3:6] *= 180.0 / np.pi
+
+    log_error = log_est - log_ans
     log_mse_acc = np.zeros(log_error.shape[0])
     log_mse_ang = np.zeros(log_error.shape[0])
     for row in range(log_error.shape[0]):
@@ -245,11 +240,14 @@ def main():
         log_mse_ang[row] = (np.dot(log_error[row, 3:], log_error[row, 3:].T))
     log_mse_acc = np.sqrt(log_mse_acc)
     log_mse_ang = np.sqrt(log_mse_ang)
+    log_mse_ang_avg = [np.median(log_mse_ang[i:i+5]) for i in range(log_mse_ang.shape[0]-5)]
+    log_mse_ang_avg = np.hstack((np.zeros(5), np.array(log_mse_ang_avg)))
 
-    plt.rcParams['font.size'] = 20
-    plt.rcParams['font.family'] = 'sans-serif'
-    plt.rcParams['font.sans-serif'] = 'Arial'
-    plt.rcParams['lines.linewidth'] = 2
+    log_est = np.array(log_est)
+    log_ans = np.array(log_ans)
+
+    log_est[3:6] *= 180.0 / np.pi
+    log_ans[3:6] *= 180.0 / np.pi
 
     fig0, ax = plt.subplots(nrows=2, sharex=True, figsize=(16,12))
     ax[0].plot(log_est_t,np.array(log_est)[:,0], label='est_x')
@@ -273,7 +271,12 @@ def main():
     ax[1].set_ylabel(r'$Angular\ Acceleration\ [deg/s^{2}]$')
 
     ax[1].set_xlabel(r'$Time[s]$')
-    fig0.savefig('./model/'+file_date+'_log_est.png')
+    ax_time = time
+    while ax_time > 500:
+        ax[1].set_xlim([(ax_time-500)*0.01, ax_time*0.01])
+        fig0.savefig('./model/'+file_date+'_log_est_'+str(ax_time)+'.png')
+        ax_time -= 1000
+    fig0.savefig('./model/'+file_date+'_log_est'+test_object+'.png')
 
 
     fig1, ax1 = plt.subplots(nrows=2, sharex=True, figsize=(16,12))
@@ -289,7 +292,7 @@ def main():
     ax1[1].set_ylabel(r'$Control\ input$')
 
     ax1[1].set_xlabel(r'$Time[s]$')
-    fig1.savefig('./model/'+file_date+'_log_mse.png')
+    fig1.savefig('./model/'+file_date+'_log_mse'+test_object+'.png')
 
     # compare simulation from estimated value and answer
     log_est_s = np.array(log_est_s)
@@ -319,11 +322,11 @@ def main():
     while time > 500:
         ax2[1].set_xlim([(time-500)*0.01, time*0.01])
         ax2[0].set_ylim([-3, 3])
-        fig2.savefig('./model/'+file_date+'_comp_sim_ans_'+str(time)+'.png')
+        fig2.savefig('./model/'+file_date+'_comp_sim_ans_'+str(time)+test_object+'.png')
         time -= 1000
     ax2[1].set_xlim([0.0, 5.0])
     ax2[0].set_ylim([-3, 3])
-    fig2.savefig('./model/'+file_date+'_comp_sim_ans_'+str(time)+'.png')
+    fig2.savefig('./model/'+file_date+'_comp_sim_ans_'+str(time)+test_object+'.png')
 
     plt.show()
     # --- Visulize the performance of the estimator ---
